5548c5af46b5566679b92ae3727b450a8d1091c3
andresds <andre.souza@asapcred.com.br>
2024-11-11 17:17:11 -0300

    Implement authentication and sign out functionality
    - Add authenticate function in actions.ts to handle user login
    - Implement sign out functionality in sidenav.tsx
    - Update login-form.tsx to use authentication and display errors
    - Upgrade next-auth to version 5.0.0-beta.25
    - Update related dependencies in package.json and pnpm-lock.yaml

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,27 @@ import { sql } from "@vercel/postgres";
 import { CreateInvoice, UpdateInvoice } from "./definitions";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
+import { AuthError } from "next-auth";
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
+}
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
